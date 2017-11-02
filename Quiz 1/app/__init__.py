1c41183220c93348d1b5600f887c8fb060b18183
Roderick Vergara <roderickjaffeth@gmail.com>
2017-11-02 01:48:16 -0500

    Palabra Secreta Quiz #1

--- Quiz 1/app/__init__.py
+++ Quiz 1/app/__init__.py
@@ -1,16 +1,12 @@
 palabra_secreta = "python"
+
 for x in range(3):
+
         palabra = input("Ingresa la palabra secreta: ")
-        if palabra == palabra_secreta.upper():
+        if palabra_secreta.upper() == palabra.upper():
             print("GANASTE!!!")
             break
-        elif palabra == palabra_secreta:
-            print("GANASTE!!!")
-            break
-        elif palabra == "Python":
-            print("GANASTE!!!")
-            break
-        elif  palabra != palabra_secreta and x <= 1:
-            print("Palabra incorrecta... Intentalo nuevamente")
+        elif palabra_secreta.upper() != palabra.upper() and x <= 1:
+            print("Palabra incorrecta vuelva a intentarlo")
         else:
-            print("Has perdido...")
+            print("Has perdido :'(")
